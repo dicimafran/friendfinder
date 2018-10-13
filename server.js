fc5f1c6845aa051b8ce8473eb1a3f3b860ff0d9f
dicimafran <36396450+dicimafran@users.noreply.github.com>
2018-10-12 21:57:56 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,74 +1,6 @@
-
-/* ============ Pseudocode ===============
-    Inputs: 
-        Form data text input
-        drop=down sorta menu ==> will be using materialize's select class
-        Submit button
-
-    Process:
-        each person's inputs will be pushed into an array.
-        A module of this array will be exported as friends.js
-
-            var person = [ 
-                    {
-                    name:
-                    photo:
-                    scores: [0-9]
-                    }; 
-                ];
-
-    Output: 
-        In the API route,  
-            
-               
-============ Table of contents====================
-
-    =====================
-    Server Components
-    =====================
-
-        Express App setup: 
-                app and port variables
-                app setup to handle data parsing
-        Router: Gives the server a route to travel to when users visit or request data from various URLS.
-        Port listening: starts the server
-
-    ===================
-    Routes Components
-    ===================
-
-     public directory: 
-        home route:               
-            It has a button leading to the survey
-            At the bottom is a list of links leading to the api and githbu repo
-        
-        survey route:
-            It has a floating button which sticks at the lower right so you can go to the home html at any time.
-
-        assets
-            css
-            images
-            js
-
-
-    routing directory
-        api route
-        html
-
-    ===================
-    ===================
-
-     ===================
-    ===================
-    
-    
-   
-*/
-
 // ============= Dependencies ===================
-
 var express= require('express');
-var bodyParse= require('body=parser');
+var bodyParser= require('body-parser');
 var path= require('path');
 
 
@@ -77,12 +9,13 @@ var path= require('path');
 var app =express();
 var PORT= process.env.PORT || 8080;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// For some reason, I had errors when bodyparser extended was true
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // ================ Router ===================
 
-require('./routes/apiRoutes') (app);
+require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
 // ============= Port Listener ==================
@@ -91,3 +24,18 @@ app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
   });
   
+
+/* ============ Summary of imports and exports ===============
+
+    Imports for server.js:
+        package dependencies
+        /app/data/friends.js
+        /app/routing/apiRoutes.js
+
+    Exports to express app: routes
+        api routes: references friends array and handles pushing new friends into array
+        html routes:  handles when users visit a page and shows html page of content (based on requests)
+
+    Exports to api routes: 
+        data/friends.js: stores the data from the friend array
+*/
